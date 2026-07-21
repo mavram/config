6aaff606b96f21500a1b60dcef8a9da8ecd25a63
mircea <mavram@gmail.com>
2026-07-21 10:54:33 -0400

    neovim options

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,24 @@
+vim.g.mapleader = " "
+vim.g.netrw_banner = 0
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.inccommand = "nosplit"
+vim.opt.termguicolors = true
 
-vim.g.mapleader = " "
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undodir"
+vim.opt.undofile = true
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down in buffer with cursor centered" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up in buffer with cursor centered" })
+
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result cursor centered" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result cursor centered" })
